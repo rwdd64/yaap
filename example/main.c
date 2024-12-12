1349bbc2518bafe2a0214c30ddec5545b230cfb9
Renan <renanfpompermayer@outlook.com>
2024-12-12 13:48:25 -0300

    Added example
    Intended to show the usage of the library

---
+++ example/main.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "..\yaap.h"
+
+int main(int argc, char** argv) {
+    int a;
+    char* b = NULL;
+
+    yaap_parser_t parser = {
+        .style = GNU_STYLE,
+        .max_options = 16
+    };
+    
+    yaap_parser_init(&parser);
+
+    yaap_reg_opt(&parser, "--opt1", OPT_BOOL, &a);
+    yaap_reg_opt(&parser, "-a", OPT_BOOL, &a);
+    yaap_reg_opt(&parser, "--opt2", OPT_VALUE, &b);
+    yaap_reg_opt(&parser, "-b", OPT_VALUE, &b);
+
+    yaap_run(&parser, argc, argv);
+
+    yaap_parser_clean(&parser);
+
+    printf("Values\n");
+    printf("============\n");
+    printf("Option 1: %d\n", a);
+    printf("Option 2: %s\n", b==NULL ? "Null" : b);
+
+    return EXIT_SUCCESS;
+}
